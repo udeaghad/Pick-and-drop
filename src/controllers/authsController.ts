9b3d49d88cda090da3f9809e9d630df151b2bd8a
udeaghad <dozie.udeagha@gmail.com>
2023-01-18 17:31:50 +0100

    create company registration auths and login auth

---
+++ src/controllers/authsController.ts
@@ -0,0 +1,69 @@
+import CompanyModel from "../models/CompanyModel";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import {Request, Response, NextFunction} from "express";
+
+interface RegisterCompanyType {
+  _id?: string;
+  name: string;
+  email: string;
+  phoneNumber: number;
+  city: string;
+  state: string;
+  password: string;
+  logo: string;
+  rating?:number;
+};
+
+interface LoginCompanyType {
+  email: string;
+  password: string;
+}
+
+const jwtEnv: string = String(process.env.JWT);
+
+
+
+export const registerCompany = async(req:Request, res:Response, next:NextFunction): Promise<void> => {
+  const {name, email, phoneNumber, city, state, password, logo}: RegisterCompanyType = req.body;
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt)
+
+    const newCompany = new CompanyModel<RegisterCompanyType>({
+      name,
+      email,
+      phoneNumber,
+      city,
+      state,
+      password: hash,
+      logo,
+    })
+
+    await newCompany.save();
+    res.status(201).send("User created successfully")
+  } catch (err) {
+    next(err);
+  }
+}
+
+export const companyLogin = async(req: Request, res: Response, next: NextFunction) => {
+  
+  try {
+    const company: RegisterCompanyType | null = await CompanyModel.findOne({email: req.body.email});
+    if(!company) return {status: 404, message: "Company not found"};
+
+    const validPassword = await bcrypt.compare(req.body.password, company.password);
+    if(!validPassword) return {status: 404, message: "Invalid Password"}
+
+    const token = jwt.sign({id: company._id}, jwtEnv)    
+
+    const {password, ...otherDetails } = company;
+
+    res
+    .cookie("cookies", token, { httpOnly: true, sameSite: "none", secure: true})
+    .status(200).json(otherDetails);
+  } catch (err) {
+    next(err)
+  }
+}
