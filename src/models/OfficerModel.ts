f2ac2d46713d52b5ceb59bec3a41eba4cbaa4ef4
udeaghad <dozie.udeagha@gmail.com>
2023-01-20 17:50:32 +0100

    update officer model

--- src/models/OfficerModel.ts
+++ src/models/OfficerModel.ts
@@ -1,4 +1,5 @@
 import mongoose, { Types } from "mongoose";
+import { type } from "os";
 
 const { Schema } = mongoose;
 
@@ -8,4 +9,53 @@ interface IOfficer {
   companyId: Types.ObjectId;
   location: string;
   phoneNumber: string;
+  pending: number;
+  viewed: number;
+  picked: number;
+  transit: number;
 }
+
+const OfficerSchema = new Schema<IOfficer>({
+  name: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
+
+  address: {
+    type: String,
+    required: true,
+  },
+
+  companyId: {
+    type: Schema.Types.ObjectId, ref: "Company", requird: true
+  },
+
+  location: {
+    type: String,
+  },
+
+  phoneNumber: {
+    type: String,
+  },
+
+  pending: {
+    type: Number,
+    default: 0,
+  },
+  viewed: {
+    type: Number,
+    default: 0,
+  },
+  picked: {
+    type: Number,
+    default: 0,
+  },
+  transit: {
+    type: Number,
+    default: 0,
+  },
+},
+{timestamps: true});
+
+export default mongoose.model("Officer", OfficerSchema);
