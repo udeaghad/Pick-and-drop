30f816e616e316df5e593ef95883c93d06551924
udeaghad <dozie.udeagha@gmail.com>
2023-01-20 16:54:14 +0100

    Handle route for updateCompany

--- src/routes/companyRoutes.ts
+++ src/routes/companyRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllCompanies, getCompany } from "../controllers/companyController";
+import { getAllCompanies, getCompany, updateCompany } from "../controllers/companyController";
 
 const router = express.Router();
 
 router.get("/", getAllCompanies);
-router.get("/:companyId", getCompany)
+router.get("/:companyId", getCompany);
+router.put("/:companyId", updateCompany);
 
 export default router;
