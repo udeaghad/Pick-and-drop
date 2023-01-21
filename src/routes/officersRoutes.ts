db49ae4b1e972b71599dc7770ddad7f4745a1442
udeaghad <dozie.udeagha@gmail.com>
2023-01-21 20:25:19 +0100

    Add routes for deleting officer

--- src/routes/officersRoutes.ts
+++ src/routes/officersRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { getAllOfficers, getOfficer, updateOfficer } from "../controllers/officersController"
+import { deleteOfficer, getAllOfficers, getOfficer, updateOfficer } from "../controllers/officersController"
 
 const router = express.Router();
 
 router.get("/companies/:companyId", getAllOfficers);
-router.get("/:officerId/companies/:companyId", getOfficer)
-router.put("/:officerId", updateOfficer)
+router.get("/:officerId/companies/:companyId", getOfficer);
+router.put("/:officerId", updateOfficer);
+router.delete("/:officerId", deleteOfficer);
 
 export default router;
