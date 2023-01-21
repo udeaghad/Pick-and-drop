86f91a3d4f0a1d15cb4def32a6ffd0783409a977
udeaghad <dozie.udeagha@gmail.com>
2023-01-21 19:55:35 +0100

    Setup get officer routes

--- src/routes/officersRoutes.ts
+++ src/routes/officersRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAllOfficers } from "../controllers/officersController"
+import { getAllOfficers, getOfficer } from "../controllers/officersController"
 
 const router = express.Router();
 
-router.get("/:companyId", getAllOfficers);
+router.get("/companies/:companyId", getAllOfficers);
+router.get("/:officerId/companies/:companyId", getOfficer)
 
 export default router;
