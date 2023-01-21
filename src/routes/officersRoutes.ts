9c56008c9bef4a2ddf1882f88d1a38f5eec82569
udeaghad <dozie.udeagha@gmail.com>
2023-01-21 20:06:41 +0100

    Setup updateOfficer routes

--- src/routes/officersRoutes.ts
+++ src/routes/officersRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllOfficers, getOfficer } from "../controllers/officersController"
+import { getAllOfficers, getOfficer, updateOfficer } from "../controllers/officersController"
 
 const router = express.Router();
 
 router.get("/companies/:companyId", getAllOfficers);
 router.get("/:officerId/companies/:companyId", getOfficer)
+router.put("/officerId", updateOfficer)
 
 export default router;
