16b520e33c7585a38bfe34733f6b13fcff63765f
udeaghad <dozie.udeagha@gmail.com>
2023-01-21 19:15:10 +0100

    setup routes for officer password update

--- src/routes/authsRoutes.ts
+++ src/routes/authsRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerCompany, companyLogin, updateCompanyPassword } from "../controllers/authsController";
-import { officerLogin, registerOfficer} from "../controllers/authsController";
+import { officerLogin, registerOfficer, updateOfficerPassword} from "../controllers/authsController";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post("/pasword/company/:companyId", updateCompanyPassword);
 
 router.post("/register/officer", registerOfficer);
 router.post("/login/officer", officerLogin);
+router.post("password/officer/:officerId", updateOfficerPassword);
 
 export default router;
