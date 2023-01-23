e1684d05a4d94181adcd23a83b19547e14f7d325
udeaghad <dozie.udeagha@gmail.com>
2023-01-23 17:26:19 +0100

    Update route for sender update

--- src/routes/sendersRoutes.ts
+++ src/routes/sendersRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { createSender } from "../controllers/sendersController";
+import { createSender, updateSender } from "../controllers/sendersController";
 
 const router = express.Router();
 
 router.post("/", createSender)
+router.put("/:senderId", updateSender)
 
 export default router;
