f93a66ad00fa33fb17e7975c72a6906133d8d384
udeaghad <dozie.udeagha@gmail.com>
2023-01-24 14:48:00 +0100

    Update route for getSender and getReceiver

--- src/routes/receiversRoutes.ts
+++ src/routes/receiversRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createReceiver, updateReceiver } from "../controllers/receiversController"
+import { createReceiver, getReceiver, updateReceiver } from "../controllers/receiversController"
 
 const router = express.Router();
 
 router.post("/senders/:senderId", createReceiver )
 router.put("/:receiverId", updateReceiver)
+router.get("/:receiverId", getReceiver)
 
 export default router;
--- src/routes/sendersRoutes.ts
+++ src/routes/sendersRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createSender, updateSender } from "../controllers/sendersController";
+import { createSender, getSender, updateSender } from "../controllers/sendersController";
 
 const router = express.Router();
 
 router.post("/", createSender)
 router.put("/:senderId", updateSender)
+router.get("/:senderId", getSender)
 
 export default router;
