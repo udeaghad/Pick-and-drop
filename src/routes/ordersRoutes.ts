8f9f022ee9ba15be726d769b308437111afd7361
udeaghad <dozie.udeagha@gmail.com>
2023-01-24 16:44:59 +0100

    Create route for getOrder

--- src/routes/ordersRoutes.ts
+++ src/routes/ordersRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { createOrder, updateOrder } from "../controllers/ordersController";
+import { createOrder, getOrder, updateOrder } from "../controllers/ordersController";
 
 const router = express.Router();
 
 router.post("/", createOrder);
 router.put("/:orderId", updateOrder)
+router.get("/:orderId", getOrder)
 
 export default router;
