8956f6db5041a215eef2711b75322b1732aa6d2f
udeaghad <dozie.udeagha@gmail.com>
2023-02-02 17:42:05 +0100

    Create test for officer authentication

--- src/__tests__/auths.test.ts
+++ src/__tests__/auths.test.ts
@@ -36,149 +36,149 @@ const companyPayload = {
   __v:0
   }
 
-// describe("Create Company", () => {
-//   it("Should create a new company successfully!", (done) => {
-//     const companyInput = {
-//       name: "company2",
-//       email:"company2@example.com",
-//       phoneNumber: "2348080425123",
-//       city: "Onitsha",
-//       state: "Anambra",
-//       password: "mypassword"
-//     }
+describe("Create Company", () => {
+  it("Should create a new company successfully!", (done) => {
+    const companyInput = {
+      name: "company2",
+      email:"company2@example.com",
+      phoneNumber: "2348080425123",
+      city: "Onitsha",
+      state: "Anambra",
+      password: "mypassword"
+    }
      
-//      agent
-//      .post('/api/v1/auths/register/company')
-//      .send(companyInput)
-//      .expect(200)
-//      .end((err, res) => {
+     agent
+     .post('/api/v1/auths/register/company')
+     .send(companyInput)
+     .expect(200)
+     .end((err, res) => {
 
-//        expect(res.status).toEqual(200);
-//        return done();
-//      })      
+       expect(res.status).toEqual(200);
+       return done();
+     })      
     
     
-//   });
+  });
 
-//   it("Should return error if name is mising", (done) => {
-//     const companyInput = {
-//       email:"company3@example.com",
-//       phoneNumber: "2348080425123",
-//       city: "Onitsha",
-//       state: "Anambra",
-//       password: "mypassword"
-//     }
-//     agent
-//      .post('/api/v1/auths/register/company')
-//      .send(companyInput)
-//      .expect(500)
-//      .end((err, res) => {
+  it("Should return error if name is mising", (done) => {
+    const companyInput = {
+      email:"company3@example.com",
+      phoneNumber: "2348080425123",
+      city: "Onitsha",
+      state: "Anambra",
+      password: "mypassword"
+    }
+    agent
+     .post('/api/v1/auths/register/company')
+     .send(companyInput)
+     .expect(500)
+     .end((err, res) => {
 
-//        expect(res.status).toEqual(500)
-//        return done();
-//      })
+       expect(res.status).toEqual(500)
+       return done();
+     })
      
-//   })
-//   it("Should return error if email is mising", (done) => {
-//     const companyInput = {
-//       name: "company3",
-//       phoneNumber: "2348080425113",
-//       city: "Onitsha",
-//       state: "Anambra",
-//       password: "mypassword"
-//     }
-//     agent
-//      .post('/api/v1/auths/register/company')
-//      .send(companyInput)
-//      .expect(500)
-//      .end((err, res) => {
+  })
+  it("Should return error if email is mising", (done) => {
+    const companyInput = {
+      name: "company3",
+      phoneNumber: "2348080425113",
+      city: "Onitsha",
+      state: "Anambra",
+      password: "mypassword"
+    }
+    agent
+     .post('/api/v1/auths/register/company')
+     .send(companyInput)
+     .expect(500)
+     .end((err, res) => {
        
-//        expect(res.statusCode).toEqual(500)
-//        return done();
-//      })
+       expect(res.statusCode).toEqual(500)
+       return done();
+     })
      
-//   })
-//   it("Should return error if phone number is mising", (done) => {
-//     const companyInput = {
-//       name: "company5",
-//       email:"company5@example.com",
-//       city: "Onitsha",
-//       state: "Anambra",
-//       password: "mypassword"
-//     }
-//     agent
-//      .post('/api/v1/auths/register/company')
-//      .send(companyInput)
-//      .expect(500)
-//      .end((err, res) => {
+  })
+  it("Should return error if phone number is mising", (done) => {
+    const companyInput = {
+      name: "company5",
+      email:"company5@example.com",
+      city: "Onitsha",
+      state: "Anambra",
+      password: "mypassword"
+    }
+    agent
+     .post('/api/v1/auths/register/company')
+     .send(companyInput)
+     .expect(500)
+     .end((err, res) => {
        
-//        expect(res.statusCode).toEqual(500)
-//        return done();
-//      })
+       expect(res.statusCode).toEqual(500)
+       return done();
+     })
      
-//   })
-// })
+  })
+})
 
   
-// describe("Login as a company", () => {
-//   it("Should login successfully", (done) => {
-//     const loginDetails = {
-//       email: "company2@example.com",
-//       password: "mypassword"
-//     }
+describe("Login as a company", () => {
+  it("Should login successfully", (done) => {
+    const loginDetails = {
+      email: "company2@example.com",
+      password: "mypassword"
+    }
 
-//     agent
-//     .post("/api/v1/auths/login/company")
-//     .send(loginDetails)
-//     .expect(200)
-//     .end((err, res) => {
-//       expect(res.statusCode).toEqual(200);
-//       expect(res.body.name).toBe("company2")
-//       return done();
-//     })
+    agent
+    .post("/api/v1/auths/login/company")
+    .send(loginDetails)
+    .expect(200)
+    .end((err, res) => {
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.name).toBe("company2")
+      return done();
+    })
 
-//   })
+  })
 
-//   it("Should return error on wrong password", (done) => {
-//     const loginDetails = {
-//       email: "company2@example.com",
-//       password: "pasword"
-//     }
+  it("Should return error on wrong password", (done) => {
+    const loginDetails = {
+      email: "company2@example.com",
+      password: "pasword"
+    }
 
-//     agent
-//     .post("/api/v1/auths/login/company")
-//     .send(loginDetails)
-//     .expect(404)
-//     .end((err, res) => {
+    agent
+    .post("/api/v1/auths/login/company")
+    .send(loginDetails)
+    .expect(404)
+    .end((err, res) => {
 
-//       expect(res.statusCode).toEqual(404)
-//       return done();
-//     })
+      expect(res.statusCode).toEqual(404)
+      return done();
+    })
     
     
-//   })
+  })
   
-//   it("Should return error on wrong company name", (done) => {
-//     const loginDetails = {
-//       email: "company21@example.com",
-//       password: "mypassword"
-//     }
+  it("Should return error on wrong company name", (done) => {
+    const loginDetails = {
+      email: "company21@example.com",
+      password: "mypassword"
+    }
     
-//     agent
-//     .post("/api/v1/auths/login/company")
-//     .send(loginDetails)
-//     .expect(200)
-//     .end((err, res) => {
+    agent
+    .post("/api/v1/auths/login/company")
+    .send(loginDetails)
+    .expect(200)
+    .end((err, res) => {
 
-//       expect(res.statusCode).toEqual(404);
-//       expect(res.text).toBe("Account does not exist")
-//       return done()
-//     })
-//   })  
-// })
+      expect(res.statusCode).toEqual(404);
+      expect(res.text).toBe("Account does not exist")
+      return done()
+    })
+  })  
+})
 
 describe("Create Officer", () => {
-  it("Create officer account", (done) => {
+  it("Should create officer account", (done) => {
     const companyLoginDetails = {
       email: "company2@example.com",
       password: "mypassword"
@@ -212,6 +212,60 @@ describe("Create Officer", () => {
      
 
   })
+
+  it("Should login officer successfully", (done) => {
+    const officerLoginDetails = {
+      phoneNumber: "08033548",
+      password: "mypassword"
+    }
+
+    agent
+    .post("/api/v1/auths/login/company")
+    .send(officerLoginDetails)
+    .expect(200)
+    .end((err, res) => {
+      expect(res.statusCode).toEqual(200)
+      return done()
+    })
+  })
+
+  it("Should return error on wrong phone number", (done) => {
+    const officerLoginDetails = {
+      phoneNumber: "0803",
+      password: "mypassword"
+    }
+    
+        agent
+        .post("/api/v1/auths/login/company")
+        .send(officerLoginDetails)
+        .expect(404)
+        .end((err, res) => {
+    
+          expect(res.statusCode).toEqual(404)
+          return done();
+        })
+        
+        
+      })
+
+  it("Should return error on wrong password", (done) => {
+    const officerLoginDetails = {
+      phoneNumber: "08033548",
+      password: "wrongpassword"
+    }
+    
+        agent
+        .post("/api/v1/auths/login/company")
+        .send(officerLoginDetails)
+        .expect(404)
+        .end((err, res) => {
+    
+          expect(res.statusCode).toEqual(404)
+          return done();
+        })
+        
+        
+      })
 })
 
 describe("Update password", () => {
@@ -242,8 +296,41 @@ describe("Update password", () => {
         return done()
         
       })
+
+    
     })
-             
+              
+  })
+
+  it("Should update officer password successfully", (done) => {
+    const officerLoginDetails = {
+      phoneNumber: "08033548",
+      password: "mypassword"
+    }
+
+    const updateDetail = {
+      password: "mypassword"
+    }
+
+    agent
+    .post("/api/v1/auths/login/company")
+    .send(officerLoginDetails)
+    .expect(200)
+    .end((err, res) => {
+
+      agent
+      .post(`/api/v1/auths/password/officer/${res.body._id}`)
+      .set('Cookie', [res.header['set-cookie']])
+      .send(updateDetail)
+      .expect(200)
+      .end((err, res) => {        
+        expect(res.text).toBe("Password updated successfully")
+        return done()
+        
+      })
+   
+    })
+
   })
       
       
